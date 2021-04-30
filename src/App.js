7be34aeeb22c8e795ddba625541f5067a162f019
Nahuel Torres <nahu995@gmail.com>
2021-04-29 21:05:02 -0300

    complete assigment-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react'
-import { DISHES } from "./shared/dishes"
-import { Navbar, NavbarBrand } from 'reactstrap'
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -9,9 +11,10 @@ class App extends Component {
       dishes: DISHES
     };
   }
+
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
